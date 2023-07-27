179db238d2c693fdcd79c42b157c6ba18eea9106
Divyansh <divyanshsrivastav72@gmail.com>
2023-07-27 22:42:02 +0530

    imp msg

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const button = document.createElement("button");
+
+
+button.innerText = "Message";
+
+
+button.addEventListener("click", function() {
+    alert("Site will be working soon");
+});
+
+
+document.body.appendChild(button);
